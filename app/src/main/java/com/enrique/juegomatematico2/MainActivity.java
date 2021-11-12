c8856d56bb472651d7a7921e410c52f3441334b3
Enrique <fpemlktz@gmail.com>
2021-11-12 14:00:30 +0100

    fase intermedia 2

--- app/src/main/java/com/enrique/juegomatematico2/MainActivity.java
+++ app/src/main/java/com/enrique/juegomatematico2/MainActivity.java
@@ -1,23 +1,29 @@
 package com.enrique.juegomatematico2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnrd;
-    TextView tv;
+    Button btnrd,btnresultado;
+    TextView tv,tvr;
+    RadioButton rd1,rd2;
     //ToggleButton tgbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnrd=(Button) findViewById(R.id.btnrandom);
+        btnresultado=(Button) findViewById(R.id.result);
         tv=(TextView) findViewById(R.id.anio);
+        tvr=(TextView) findViewById(R.id.Resultado);
+        rd1=(RadioButton) findViewById(R.id.yes);
+        rd2=(RadioButton) findViewById(R.id.NO);
        // tgbtn=(ToggleButton) findViewById(R.id.toggleButton);
 
         btnrd.setOnClickListener(new View.OnClickListener() {
@@ -26,11 +32,24 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(String.valueOf((int)(Math.random()*(1950-2500)+2500)));
             }
         });
+
+        btnresultado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!(rd1.isChecked()&&rd2.isChecked()))
+                    Toast.makeText(getApplicationContext(), "Debe escoger una de las opciones", Toast.LENGTH_LONG).show();
+
+                if (comprobar_bisiesto(Integer.parseInt((String) tv.getText()))) {
+                    tvr.setText("Error");
+                } else {
+                    tvr.setText("Correcto");
+                }
+            }
+        });
     }
 
 
-    public boolean comprobar_bisiesto(String opc, int anio){
-        if(opc.equals("yes")&& (anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0))) return true;
-        else return false;
+    public boolean comprobar_bisiesto(int anio){
+        return (anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0));
     }
 }
